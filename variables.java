c86c475c1d93232829d9af445b2824b76a7ec015
Khushi232505 <khushiitiwari23@gmail.com>
2026-01-10 23:54:33 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,16 @@
+public class variables{
+    public static void main(String[] args) {
+        // String name = "khushi";
+        // double price = 25.25;
+        int a = 40;
+        int b =  78;
+        int sum = a+b;
+        // System.out.println(sum);
+         
+        int diff = b-a;
+        // System.out.println(diff);
+
+        int mul = (a*b)/(a-b);
+        System.out.println(mul);
+ }
+}
